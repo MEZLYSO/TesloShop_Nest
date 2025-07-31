8e8de08984d7bd58b24e7b32bd39fb7db9a8373d
Maury SO <lynuxazura@gmail.com>
2025-07-31 12:58:39 -0600

    Config .env and TypeORM

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # TesloShop_Nest
+
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="120" alt="Nest Logo" /></a>
+
+
+# TESLO API
+
+1. Clone the proyect 
+
+2. ```yarn install```
+
+3. CLone the file ```.env.template``` rename with ```.env```
+
+4. Change the enviroment variables
+
+5. Up to the database 
+```
+docker compose up -d
+```
+
+6. Start proyect ```yarn start:dev```
--- teslo-shop/src/app.module.ts
+++ teslo-shop/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      // Cargue automaticmante las entidades
+      autoLoadEntities: true,
+      // No recomendado en produccion
+      synchronize: true
+    })
+  ]
 })
 export class AppModule { }
