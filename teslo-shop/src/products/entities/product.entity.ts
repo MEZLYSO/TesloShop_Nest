296f90c4852512aca41c3b3ecf3b1f00e5f74dd0
Maury SO <lynuxazura@gmail.com>
2025-07-31 15:27:15 -0600

    Get ome by id and Delete implement

--- teslo-shop/src/products/entities/product.entity.ts
+++ teslo-shop/src/products/entities/product.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
 @Entity()
 export class Product {
@@ -10,7 +10,7 @@ export class Product {
     { unique: true })
   title: string
 
-  @Column('numeric', {
+  @Column('float', {
     default: 0
   })
   price: number
@@ -39,9 +39,20 @@ export class Product {
   })
   sizes: string[]
 
-  @Column({
-    type: 'text'
-  })
+  @Column('text')
   gender: string
+
+  // Verificacion antes de insertar en la base de datos 
+  // usando el decorador utilizado
+  @BeforeInsert()
+  checkSlugInsert() {
+    if (!this.slug) {
+      this.slug = this.title
+    }
+    this.slug = this.slug
+      .toLowerCase()
+      .replaceAll(' ', '_')
+      .replaceAll("'", '')
+  }
 
 }
