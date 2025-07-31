05e65ba2f423d63ca1231566583499973af8a936
Maury SO <lynuxazura@gmail.com>
2025-07-31 13:17:17 -0600

    Add new EndPoint

--- teslo-shop/src/app.module.ts
+++ teslo-shop/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
@@ -16,7 +17,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       // No recomendado en produccion
       synchronize: true
-    })
+    }),
+    ProductsModule
   ]
 })
 export class AppModule { }
