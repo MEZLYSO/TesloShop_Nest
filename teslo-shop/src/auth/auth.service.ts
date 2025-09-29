308f4432b056a6d51e898793f04e1179a47ed21e
Maury SO <lynuxazura@gmail.com>
2025-09-29 08:03:18 -0600

    Add Resources and Auth EndPoint

---
+++ teslo-shop/src/auth/auth.service.ts
@@ -0,0 +1,59 @@
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class AuthService {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+  ) { }
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10)
+      })
+      await this.userRepository.save(user)
+
+      const { password: Pass, ...data } = user
+
+      return data
+    } catch (error) {
+      this.handleDbErrors(error)
+    }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true }
+    })
+
+    if (!user) {
+      throw new UnauthorizedException('Credentials are not valid (email)')
+    }
+
+    if (bcrypt.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Credentials are not valid (password)')
+    }
+
+    return user
+  }
+
+  private handleDbErrors(error: any): never {
+    if (error.code === '23505') {
+      throw new BadRequestException(error.detail)
+    }
+    throw new InternalServerErrorException('Check server logs')
+  }
+}
---
+++ teslo-shop/src/auth/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('users')
+export class User {
+
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({
+    type: 'text',
+    unique: true
+  })
+  email: string
+
+  @Column('text', {
+    select: false
+  })
+  password: string
+
+  @Column('text')
+  fullname: string
+
+  @Column({
+    type: 'bool',
+    default: true
+  })
+  isActive: boolean
+
+  @Column({
+    type: 'text',
+    array: true,
+    default: ['user']
+  })
+  roles: string[]
+}
